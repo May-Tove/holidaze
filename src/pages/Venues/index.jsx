8806c6ef69696574b23e976275ea9bfcd9a03bc3
May-Tove <89157761+May-Tove@users.noreply.github.com>
2023-04-27 18:49:21 +0200

    Adding venue list and filters to the venue page

--- src/pages/Venues/index.jsx
+++ src/pages/Venues/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import VenuesList from '../../components/VenuesList';
+import Filters from '../../components/Filters';
 
 export const Venues = () => {
   return (
-    <main>
-      <h1>Venues</h1>
+    <main className="w-5/6 m-auto lg:w-4/5 flex flex-col lg:flex-row gap-5">
+      <Filters />
+      <VenuesList />
     </main>
   );
 };
