c7fdd9e6e45e5a58de9e5fb1ba95db4a79773e09
May-Tove <89157761+May-Tove@users.noreply.github.com>
2023-04-30 20:15:56 +0200

    Adding content to the login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import LoginForm from '../../components/Auth/Login';
 
 export const Login = () => {
   return (
-    <main>
-      <h1>Login</h1>
+    <main className="w-5/6 m-auto lg:w-4/5 py-40 ">
+      <LoginForm />
     </main>
   );
 };
