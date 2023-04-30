27d1fed03f98ad2fc9b88a8f1f452a6fcdd93750
May-Tove <89157761+May-Tove@users.noreply.github.com>
2023-04-30 20:19:18 +0200

    Adding some basics to the profile page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import ProfileDetails from '../../components/Profile/Customer';
 
 export const Profile = () => {
   return (
-    <main>
-      <h1>Profile</h1>
+    <main className="w-5/6 m-auto lg:w-4/5 py-40">
+      <ProfileDetails />
     </main>
   );
 };
