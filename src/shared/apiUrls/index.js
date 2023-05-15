ccbf94e001fc62bb6bf32ddfdc2eb0e8e7355ebd
May-Tove <89157761+May-Tove@users.noreply.github.com>
2023-05-15 20:18:55 +0200

    Setting url and regex in shared constants

---
+++ src/shared/apiUrls/index.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://api.noroff.dev/api/v1/holidaze';
+export const API_AUTH_URL = `${API_URL}/auth`;
+export const API_VENUE_URL = `${API_URL}/venues`;
+export const API_PROFILE_URL = `${API_URL}/profiles`;
+export const API_BOOKINGS_URL = `${API_URL}/bookings`;
