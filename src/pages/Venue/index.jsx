d1e7145baba85c8f5b29118b41b6943dd3efcc8f
May-Tove <89157761+May-Tove@users.noreply.github.com>
2023-05-19 16:48:28 +0200

    Moved rendering of single venue/clean up code

--- src/pages/Venue/index.jsx
+++ src/pages/Venue/index.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import useAxiosFetch from '../../hooks/useAxiosFetch';
+import VenueDetailsLoader from '../../components/Loaders/VenueDetailsLoader';
 import VenueDetails from '../../components/Venue/VenueDetails';
+import { API_VENUE_URL } from '../../shared';
 
 export const Venue = () => {
+  let { id } = useParams();
+
+  const { data, isLoading, isError } = useAxiosFetch(
+    `${API_VENUE_URL}/${id}?_owner=true&_bookings=true`
+  );
+
+  if (isError || !data) {
+    return <div>Error</div>;
+  }
+
+  if (isLoading) {
+    return (
+      <main className="main-layout">
+        <VenueDetailsLoader />
+      </main>
+    );
+  }
+
   return (
-    <main className="py-40 w-5/6 m-auto lg:w-4/5 max-w-[1400px]">
-      <VenueDetails />
+    <main className="main-layout">
+      <VenueDetails venue={data} />
     </main>
   );
 };
